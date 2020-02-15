0ee379e2ae6271375de9595a96491b9b6ede1abe
Chris Farmer <cfarmer@zipcodes-MacBook-Pro.local>
2020-02-15 12:12:37 -0500

    finished selection method and passed tests

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -9,6 +9,49 @@ public class Music {
     }
 
     public Integer selection(Integer startIndex, String selection){
-        return null;
+        Integer forward = 0;
+        Integer backward = 0;
+        boolean foundForward = false;
+        boolean foundBackward = false;
+        for (int index = startIndex; index < playList.length; index++) {
+            if (selection != playList[index]) {
+                forward++;
+            } else {
+                foundForward = true;
+                break;
+            }
+        }
+        if (!foundForward) {
+            for (int index = 0; index < playList.length; index++) {
+                if (selection != playList[index]){
+                    forward++;
+                }
+                else {
+                    break;
+                }
+            }
+        }
+        for (int index = startIndex; index > -1; index--) {
+            if (selection != playList[index]) {
+                backward++;
+            } else {
+                foundBackward = true;
+                break;
+            }
+        }
+        if (!foundBackward) {
+            for (int index = playList.length - 1; index > -1; index--) {
+                if (selection != playList[index]){
+                    backward++;
+                }
+                else {
+                    break;
+                }
+            }
+        }
+        if (forward < backward){
+            return forward;
+        }
+        return backward;
     }
 }
